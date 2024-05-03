ef5d877826858ddfce7ee522363f2dcf58ce47dd
yordany <joldanarias@gmail.com>
2024-05-03 10:18:33 -0400

    added app ad css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
+import Form from './comps/Form';
+import List from './comps/List';
+
 
 function App() {
+const [item, setItem] = useState({item: "", price: 0, id: 1});
+const [item_list, set_item_list] = useState([]);
+
+
+
+
+  //functions and components 
+  const onSubmit = e => {
+    e.preventDefault()
+
+    set_item_list((prevValues)=> (
+        [...prevValues, item]
+    ))
+console.log(item_list)
+    
+}
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+        <Form item = {item}
+        setItem = {setItem}
+        set_item_list={set_item_list}
+        onSubmit={onSubmit} />
+
+        <List item_list = {item_list}
+        set_item_list ={set_item_list} />
+      
     </div>
   );
 }
